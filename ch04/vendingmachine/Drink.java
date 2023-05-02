37abdbed5453f0501847471e4804ada2bb38e7d6
Kim Jae-Kyeong <worud4227@naver.com>
2023-05-02 17:35:22 +0900

    VendingMachine

---
+++ ch04/vendingmachine/Drink.java
@@ -0,0 +1,32 @@
+package com.green.java.ch04.vendingmachine;
+
+public class Drink {
+
+    private int price;
+    private String name;
+    private String company;
+    private int ml;
+
+    public Drink (int price, String name, String company, int ml) {
+        this.price = price;
+        this.name = name;
+        this.company = company;
+        this.ml = ml;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getMl() {
+        return ml;
+    }
+}
